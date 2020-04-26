365cb07c83ba85e27907eb9ea0936348f92b4e1f
Ben Mills <bengeorgemills@gmail.com>
2020-04-26 20:08:43 +0100

    Comments added to Test 2 Scripts

--- Test 2 - OSPF Configuration/netmiko_ospf.py
+++ Test 2 - OSPF Configuration/netmiko_ospf.py
@@ -1,7 +1,9 @@
 import netmiko, time, re
 
+#Start in-code timer
 start = time.time()
 
+#Router connection details
 three_routers = [{
     "host":"192.168.0.1",
     "username":"admin",
@@ -19,32 +21,45 @@ three_routers = [{
     "device_type":"cisco_ios"
     }]
 
+#Connection Loop Starts
 for router in three_routers:
 
+    #Connect to the router
     connected = netmiko.ConnectHandler(**router)
 
+    #Start OSPF command list
     ospf_set = ["router ospf 1"]
 
+    #Get the connected networks
     output = connected.send_command("show ip route connected")
 
+    #Find the networks from the response using regular expression
     networks = re.findall(r"\d+\.\d+\.\d+\.\d+/\d\d", output)
 
+    #loop through the networks
     for network in networks:
 
+        #Seperate the network address from the subnet mask
         net_ip = network.split("/")[0]
 
         slash_mask = int(network.split("/")[1])
 
+        #Convert Slash notation subnet mask to binary
         bin_mask = ("0" * slash_mask) + ("1" * (32 - slash_mask))
 
+        #Convert Binary in to IP format Subnet mask
         new_mask = [int(bin_mask[:8], 2), int(bin_mask[8:16], 2), int(bin_mask[16:24], 2), int(bin_mask[24:], 2)]
 
         new_mask = list(map(str, new_mask))
 
+        #Add the network command to the OSPF command list
         ospf_set.append("network " + net_ip + " " + ".".join(new_mask) + " area 0\n")
     
+    #Send the command list to the router
     connected.send_config_set(ospf_set)
-
+    
+    #Disconnect from the router
     connected.disconnect()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
--- Test 2 - OSPF Configuration/paramiko_ospf.py
+++ Test 2 - OSPF Configuration/paramiko_ospf.py
@@ -1,7 +1,9 @@
 import paramiko, time, re
 
+#Start in-code timer
 start = time.time()
 
+#Router connection details
 three_routers = [{
     "host":"192.168.0.1",
     "username":"admin",
@@ -16,47 +18,68 @@ three_routers = [{
     "password":"admin"
     }]
 
+#Creat Paramiko SSH connection client
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+#Connection Loop Starts
 for router in three_routers:
 
+    #Connect to the router
     client.connect(router['host'], username=router['username'], password=router['password'])
 
+    #Create SSH shell
     connected = client.invoke_shell()
 
+    #Send the necessary command
     connected.send("show ip route connected\n")
 
     output = ""
     time.sleep(1)
+
+    #Get the connected networks
     while not connected.recv_ready():
         continue
 
     while connected.recv_ready():
         output = output + connected.recv(65535).decode('utf-8')
 
+    #Find the networks from the response using regular expression
     networks = re.findall(r"\d+\.\d+\.\d+\.\d+/\d\d", output)
 
+    #Go to correct router configuration level
     connected.send("configure terminal\n")
 
+    #Start OSPF configuration
     connected.send("router ospf 1\n")
 
+    #loop through the networks
     for network in networks:
 
+        #Seperate the network address from the subnet mask
         net_ip = network.split("/")[0]
 
         slash_mask = int(network.split("/")[1])
 
+        #Convert Slash notation subnet mask to binary
         bin_mask = ("0" * slash_mask) + ("1" * (32 - slash_mask))
 
+        #Convert Binary in to IP format Subnet mask
         new_mask = [int(bin_mask[:8], 2), int(bin_mask[8:16], 2), int(bin_mask[16:24], 2), int(bin_mask[24:], 2)]
 
         new_mask = list(map(str, new_mask))
 
+        #Send the network command to the router
         connected.send("network " + net_ip + " " + ".".join(new_mask) + " area 0\n")
+    
+    #Wait to make sure command has sent before disconnecting
     time.sleep(1)
+
+    #Disconnect from the router
     connected.close()
 
+#Close Paramiko SSH connection client
 client.close()
 
+#Print finished time to the screen
 print('{:.3f}'.format(time.time() - start))
